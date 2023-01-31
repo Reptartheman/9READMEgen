6b60a51e1eb28c9d6f8aae9faa2ef2f0ea396dae
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-01-31 16:35:59 -0500

    commit with questions array

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-// TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of your project.'
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Who is the author of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project',
+    },
+    {
+        type: 'confirm',
+        name: 'license',
+        message: 'What kind of license is your project?',
+    },
+];
+//console.log(questions);
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
